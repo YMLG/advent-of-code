78d9d8121f2a26dfca6170ccee60292f7257cec1
Le Gueut <Yves-Marie>
2018-12-13 14:12:30 +0100

    day 13

---
+++ src/main/java/advent/of/code/day13/Position.java
@@ -0,0 +1,90 @@
+package advent.of.code.day13;
+
+public class Position  implements Comparable<Position>{
+  
+    private int x;
+    
+    private int y;
+    
+    private char c;
+
+    
+    
+    public Position(int x, int y, char c) {
+      this.x = x;
+      this.y = y;
+      if('v' == c || '^' == c) {
+        this.c = '|';
+      }else if('>' == c || '<' == c) {
+        this.c = '-';
+      }else {
+        this.c = c;
+      }
+    }
+
+    public Position(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    public int getX() {
+      return x;
+    }
+
+    public void setX(int x) {
+      this.x = x;
+    }
+
+    public int getY() {
+      return y;
+    }
+
+    public void setY(int y) {
+      this.y = y;
+    }
+
+    public char getC() {
+      return c;
+    }
+
+    public void setC(char c) {
+      this.c = c;
+    }
+
+    @Override
+    public int hashCode() {
+      final int prime = 31;
+      int result = 1;
+      result = prime * result + x;
+      result = prime * result + y;
+      return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj)
+        return true;
+      if (obj == null)
+        return false;
+      if (getClass() != obj.getClass())
+        return false;
+      Position other = (Position) obj;
+      if (x != other.x)
+        return false;
+      if (y != other.y)
+        return false;
+      return true;
+    }
+
+    @Override
+    public String toString() {
+      return x + "," + y;
+    }
+
+    @Override
+    public int compareTo(Position o) {
+      return toString().compareTo(o.toString());
+    }
+    
+    
+}
---
+++ src/main/java/advent/of/code/day13/Voiture.java
@@ -0,0 +1,123 @@
+package advent.of.code.day13;
+
+import java.util.List;
+
+public class Voiture implements Comparable<Voiture>{
+
+  private Position position;
+  
+  private char direction;
+  
+  private int nbTurn = 0;
+
+  public Voiture(int x, int y , char direction) {
+    this.position = new Position(x, y);
+    this.direction = direction;
+  }
+
+  public Position getPosition() {
+    return position;
+  }
+
+  public void setPosition(Position position) {
+    this.position = position;
+  }
+
+  public char getDirection() {
+    return direction;
+  }
+
+  public void setDirection(char direction) {
+    this.direction = direction;
+  }
+  
+  public void run(List<Position> positions) {
+    updatePosition();
+    updateDirection(positions);
+  }
+
+  private void updatePosition() {
+    switch (direction) {
+    case '^':
+      position.setY(position.getY()-1);
+      break;
+    case 'v':
+      position.setY(position.getY()+1);
+      break;
+    case '>':
+      position.setX(position.getX()+1);
+      break;
+    case '<':
+      position.setX(position.getX()-1);
+      break;
+    default:
+      break;
+    }
+  }
+
+  private void updateDirection(List<Position> positions) {
+    char newP = positions.stream().filter(p -> position.equals(p)).map(Position::getC).findFirst().get();
+    if(newP == '/' && direction == '^') {
+      direction = '>';
+    }
+    else if(newP == '/' && direction == 'v') {
+      direction = '<';
+    }
+    else if(newP == '/' && direction == '>') {
+      direction = '^';
+    }
+    else if(newP == '/' && direction == '<') {
+      direction = 'v';
+    }
+    else if(newP == '\\' && direction == '^') {
+      direction = '<';
+    }
+    else if(newP == '\\' && direction == 'v') {
+      direction = '>';
+    }
+    else if(newP == '\\' && direction == '>') {
+      direction = 'v';
+    }
+    else if(newP == '\\' && direction == '<') {
+      direction = '^';
+    }else if(newP == '+') {
+      if(nbTurn == 0) {
+        switch (direction) {
+        case '^':
+          direction = '<';
+          break;
+        case 'v':
+          direction = '>';
+          break;
+        case '<':
+          direction = 'v';
+          break;
+        case '>':
+          direction = '^';
+          break;
+        }
+      }else if(nbTurn == 2) {
+        switch (direction) {
+        case '^':
+          direction = '>';
+          break;
+        case 'v':
+          direction = '<';
+          break;
+        case '<':
+          direction = '^';
+          break;
+        case '>':
+          direction = 'v';
+          break;
+        }
+      }
+      nbTurn = (nbTurn+1) % 3  ;
+    }
+  }
+
+  @Override
+  public int compareTo(Voiture o) {
+    return position.compareTo(o.getPosition());
+  }
+}
